ae144ac054e76ce7b22a6eb6cceee6a63b29031d
Martin Siregar <72garmartin@gmail.com>
2023-12-06 11:39:43 +0700

    52. Membuat User Controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function login() {
+
+    }
+
+    public function doLogin() {
+
+    }
+
+    public function doLogout() {
+
+    }
+}
