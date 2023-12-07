8c7af75c78990364f71e8b7b23d21cb71f1287b9
Martin Siregar <72garmartin@gmail.com>
2023-12-07 15:06:22 +0700

    63. Membuat Todolist Controller

---
+++ app/Http/Controllers/TodolistController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TodolistController extends Controller
+{
+    
+    public function todoList(Request $request) {
+
+    }
+
+    public function addTodo(Request $request) {
+
+    }
+
+    public function removeTodo(Request $request, string $todoId) {
+
+    }
+}
